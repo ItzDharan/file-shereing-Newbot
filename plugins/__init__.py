9b90ef1652cc4bc0ce8441398d5edcc1f4e84726
ItzDharan <90498910+ItzDharan@users.noreply.github.com>
2024-03-28 14:19:07 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,15 +1,15 @@
+#(©)Codexbotz
+#@iryme
+
+
+
+
+
 from aiohttp import web
 from .route import routes
 
 
-async def create_web_app():
-    """
-    Create and configure the aiohttp web application.
-    """
-    # Create a new web application instance with a maximum client size of 30MB
+async def web_server():
     web_app = web.Application(client_max_size=30000000)
-
-    # Add routes to the web application
     web_app.add_routes(routes)
-
     return web_app
